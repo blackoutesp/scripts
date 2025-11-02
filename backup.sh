4186b7056561c35cb6826c0ae2fc2a2a286fc25f
blackoutesp <yxion10101@gmail.com>
2025-11-02 17:12:07 +0100

    modified

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@ echo -e "[Backup script]\n\n"
 echo -e "[*] Executing backup from $home_backup to $backups_folder ..."
 
 backup_folder='/mnt/sandisk/archlinux_backups'
-backups_folder='/Datos/archlinux_backups'
+backups_folder='/Toshiba/archlinux_backups'
 home_backup='/home/blackout'
 system_backup='/'
 ignore_home_cache='/home/blackout/.cache'
@@ -18,13 +18,11 @@ else
         echo -e "[*] Parameters: $*"
         if [ $2 = $backups_folder ] && [ $1 = $home_backup ]; then
                 echo -e "[*] Selected backup destination to: $backups_folder for $home_backup...\n"
-                tar --exclude=$ignore_home_cache --exclude=$ignore_npm_cache  --exclude=$ignore_home_videos -czvpf $backups_folder/backup_$(date +%Y%m%d).tgz $home_backup
+                tar --exclude=$ignore_home_cache --exclude=$ignore_npm_cache -czvpf $backups_folder/backup_$(date +%Y%m%d).tgz $home_backup
         else
                 echo -e "[*] back up from $1 to $2"
                 tar -czvpf $2 $1
         fi
 fi
-
-# tar --exclude=$ignore_home_cache --exclude=$ignore_home_videos -czvpf /Toshiba/archlinux_backups/backup_$(date +%Y%m%d).tgz /home/blackout/
 
 #rsync -avpzf --exclude '/home/blackout/.cache/' --exclude '/home/blackout/Vídeos/' /home/blackout/ /tmp/backup_$(date +%Y%m%d).tgz
